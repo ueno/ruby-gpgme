1ff9303a32d76ad3a39e2e07235fb0dbd0543aa5
Daiki Ueno <ueno@unixuser.org>
2006-10-18 22:26:49 +0000

    Use IO callback.

--- examples/verify.rb
+++ examples/verify.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 require 'gpgme'
 
-plain = GPGME::verify(ARGF.read) do |signature|
+GPGME::verify(ARGF.read, nil, $stdout) do |signature|
   puts(signature.to_s)
 end
-puts(plain)
