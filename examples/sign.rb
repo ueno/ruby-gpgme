f9108c6fcfa5ed5620eddc0435748d8c914f23de
Daiki Ueno <ueno@unixuser.org>
2006-10-18 09:10:20 +0000

    Simplified.

--- examples/sign.rb
+++ examples/sign.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'gpgme'
 
-puts(GPGME::sign('test test test', {:mode => GPGME::GPGME_SIG_MODE_CLEAR}))
+puts(GPGME::sign('test test test', {:mode => GPGME::SIG_MODE_CLEAR}))
