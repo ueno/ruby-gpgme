ed8fff953cb91952f055c65d9398d493b4a702ee
Daiki Ueno <ueno@unixuser.org>
2009-01-16 03:12:06 +0000

    (GPGME::gpgme_get_engine_info): Store the returned info structure at the right position in the array.

--- gpgme_n.c
+++ gpgme_n.c
@@ -143,11 +143,12 @@ rb_s_gpgme_get_engine_info (VALUE dummy, VALUE rinfo)
 {
   gpgme_engine_info_t info;
   gpgme_error_t err;
+  long idx;
 
   err = gpgme_get_engine_info (&info);
   if (gpgme_err_code(err) == GPG_ERR_NO_ERROR)
     {
-      for (; info; info = info->next)
+      for (idx = 0; info; info = info->next, idx++)
 	{
 	  VALUE vinfo = rb_class_new_instance (0, NULL, cEngineInfo);
 	  rb_iv_set (vinfo, "@protocol", INT2FIX(info->protocol));
@@ -157,7 +158,9 @@ rb_s_gpgme_get_engine_info (VALUE dummy, VALUE rinfo)
 	    rb_iv_set (vinfo, "@version", rb_str_new2 (info->version));
 	  if (info->req_version)
 	    rb_iv_set (vinfo, "@req_version", rb_str_new2 (info->req_version));
-	  rb_ary_store (rinfo, 0, vinfo);
+	  if (info->home_dir)
+	    rb_iv_set (vinfo, "@home_dir", rb_str_new2 (info->home_dir));
+	  rb_ary_store (rinfo, idx, vinfo);
 	}
     }
   return LONG2NUM(err);
