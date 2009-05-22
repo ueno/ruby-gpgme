a29e54c0154886256b070962aecdc375b043d46d
Daiki Ueno <ueno@unixuser.org>
2009-05-22 11:13:15 +0000

    Simplify.

--- extconf.rb
+++ extconf.rb
@@ -8,14 +8,13 @@ end
 $CFLAGS += ' ' << `gpgme-config --cflags`.chomp
 $libs += ' ' << `gpgme-config --libs`.chomp
 
-need_workaround_keylist_next = try_run(<<'End')
+unless try_run(<<'End')
 #include <gpgme.h>
 #include <stdlib.h>
 int main (void) {
   return gpgme_check_version ("1.1.3") == NULL;
 }
 End
-unless need_workaround_keylist_next
   $CFLAGS += ' -DRUBY_GPGME_NEED_WORKAROUND_KEYLIST_NEXT'
 end
 create_makefile ('gpgme_n')
