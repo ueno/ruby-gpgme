3f649c20db2a17546f3c575a84d5c8d65e53056b
Daiki Ueno <ueno@unixuser.org>
2009-05-22 11:24:15 +0000

    Show "checking for gpgme >= 1.1.3".

--- extconf.rb
+++ extconf.rb
@@ -8,13 +8,18 @@ end
 $CFLAGS += ' ' << `gpgme-config --cflags`.chomp
 $libs += ' ' << `gpgme-config --libs`.chomp
 
-unless try_run(<<'End')
+checking_for('gpgme >= 1.1.3') do
+  if try_run(<<'End')
 #include <gpgme.h>
 #include <stdlib.h>
 int main (void) {
   return gpgme_check_version ("1.1.3") == NULL;
 }
 End
-  $CFLAGS += ' -DRUBY_GPGME_NEED_WORKAROUND_KEYLIST_NEXT'
+    true
+  else
+    $CFLAGS += ' -DRUBY_GPGME_NEED_WORKAROUND_KEYLIST_NEXT'
+    false
+  end
 end
 create_makefile ('gpgme_n')
