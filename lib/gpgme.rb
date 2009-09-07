46e238fe7e21b0579710516a9aac8e47ec61dd4b
Daiki Ueno <ueno@unixuser.org>
2009-09-07 09:30:36 +0000

    1.0.8 released.

--- lib/gpgme.rb
+++ lib/gpgme.rb
@@ -93,7 +93,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 =end
 module GPGME
-  VERSION = "1.0.7"
+  VERSION = "1.0.8"
 end
 
 require 'gpgme_n'
