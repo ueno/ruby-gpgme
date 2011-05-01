eb1e6f41128d8b323b12d28e8b8fdbd60442490d
Albert Llop <mrsimo@gmail.com>
2011-05-01 23:51:38 +0200

    Moved all GPGME::Crypto class methods to instance methods. Tests and docs updated.
    * This allows for a nicer interface with common options:
      crypto = GPGME::Crypto.new :armor => true, :password => "gpgme"
      crypto.encrypt "Some text", :symmetric => true
    * Also solves the problem of having increased the level of modules that were needed
      to call methods.

--- test/crypto_test.rb
+++ test/crypto_test.rb
@@ -1,24 +1,47 @@
 # -*- encoding: utf-8 -*-
 require 'test_helper'
 
-describe GPGME do
-  it "does the roundtrip encrypting" do
-    encrypted = GPGME::Crypto.encrypt TEXT[:plain], :always_trust => true, :recipients => KEYS.first[:sha]
-    assert_equal TEXT[:plain], GPGME::Crypto.decrypt(encrypted).read
+describe GPGME::Crypto do
+  describe "default options functionality" do
+    it "allows operation from instances normally" do
+      crypto = GPGME::Crypto.new
+      encrypted = crypto.encrypt TEXT[:plain], :always_trust => true, :recipients => KEYS.first[:sha]
+      assert_equal TEXT[:plain], crypto.decrypt(encrypted).read
+    end
+
+    it "can set default options when using the instance way" do
+      crypto = GPGME::Crypto.new :always_trust => true
+      encrypted = crypto.encrypt TEXT[:plain], :recipients => KEYS.first[:sha]
+      assert_equal TEXT[:plain], crypto.decrypt(encrypted).read
+    end
+
+    it "but they can still be overwritten" do
+      crypto = GPGME::Crypto.new :always_trust => false
+      encrypted = crypto.encrypt TEXT[:plain], :always_trust => true, :recipients => KEYS.first[:sha]
+      assert_equal TEXT[:plain], crypto.decrypt(encrypted).read
+    end
   end
 
-  it "does so even with armored encrypted stuff" do
-    encrypted = GPGME::Crypto.encrypt TEXT[:plain], :always_trust => true, :armor => true
-    assert_equal TEXT[:plain], GPGME::Crypto.decrypt(encrypted).read
+  describe "roundtrip encryption/decryption" do
+    it "does the roundtrip encrypting" do
+      crypto = GPGME::Crypto.new
+      encrypted = crypto.encrypt TEXT[:plain], :always_trust => true, :recipients => KEYS.first[:sha]
+      assert_equal TEXT[:plain], crypto.decrypt(encrypted).read
+    end
+
+    it "does so even with armored encrypted stuff" do
+      crypto = GPGME::Crypto.new
+      encrypted = crypto.encrypt TEXT[:plain], :always_trust => true, :armor => true
+      assert_equal TEXT[:plain], crypto.decrypt(encrypted).read
+    end
   end
 
   describe :encrypt do
     it "should raise an error if the recipients aren't trusted" do
       assert_raises GPGME::Error::General do
-        GPGME::Crypto.encrypt TEXT[:plain]
+        GPGME::Crypto.new.encrypt TEXT[:plain]
       end
     end
-
 
     # it "can specify which key(s) to use for encrypting with a string"
     # it "can specify which key to use for encrypting with a Key object"
@@ -31,44 +54,46 @@ describe GPGME do
   describe "symmetric encryption/decryption" do
     it "requires a password to encrypt" do
       assert_raises GPGME::Error::BadPassphrase do
-        GPGME::Crypto.encrypt TEXT[:plain], :symmetric => true
+        GPGME::Crypto.new.encrypt TEXT[:plain], :symmetric => true
       end
     end
 
     it "requires a password to decrypt" do
-      encrypted_data = GPGME::Crypto.encrypt TEXT[:plain],
+      crypto = GPGME::Crypto.new
+      encrypted_data = crypto.encrypt TEXT[:plain],
         :symmetric => true, :password => "gpgme"
 
       assert_raises GPGME::Error::BadPassphrase do
-        GPGME::Crypto.decrypt encrypted_data
+        crypto.decrypt encrypted_data
       end
     end
 
     it "can encrypt and decrypt with the same password" do
-      encrypted_data = GPGME::Crypto.encrypt TEXT[:plain],
-        :symmetric => true, :password => "gpgme"
+      crypto = GPGME::Crypto.new :symmetric => true, :password => "gpgme"
+      encrypted_data = crypto.encrypt TEXT[:plain]
+      plain = crypto.decrypt encrypted_data
 
-      plain = GPGME::Crypto.decrypt encrypted_data, :password => "gpgme"
       assert_equal "Hi there", plain.read
     end
 
     it "but breaks with different ones" do
-      encrypted_data = GPGME::Crypto.encrypt TEXT[:plain],
+      crypto = GPGME::Crypto.new
+      encrypted_data = crypto.encrypt TEXT[:plain],
         :symmetric => true, :password => "gpgme"
 
       assert_raises GPGME::Error::DecryptFailed do
-        GPGME::Crypto.decrypt encrypted_data, :password => "wrong one"
+        crypto.decrypt encrypted_data, :password => "wrong one"
       end
     end
   end
 
   describe :decrypt do
     it "decrypts encrypted stuff" do
-      assert_equal TEXT[:plain], GPGME::Crypto.decrypt(TEXT[:encrypted]).read
+      assert_equal TEXT[:plain], GPGME::Crypto.new.decrypt(TEXT[:encrypted]).read
     end
 
     it "will not get into the signatures block if there's none" do
-      GPGME::Crypto.decrypt(TEXT[:encrypted]) do |signature|
+      GPGME::Crypto.new.decrypt(TEXT[:encrypted]) do |signature|
         flunk "If I'm here means there was some signature"
       end
       pass
@@ -76,7 +101,7 @@ describe GPGME do
 
     it "will get signature elements if the encrypted thing was signed" do
       signatures = 0
-      GPGME::Crypto.decrypt(TEXT[:signed]) do |signature|
+      GPGME::Crypto.new.decrypt(TEXT[:signed]) do |signature|
         assert_instance_of GPGME::Signature, signature
         signatures += 1
       end
@@ -85,7 +110,7 @@ describe GPGME do
 
     it "writes to the output if passed" do
       buffer = GPGME::Data.new
-      GPGME::Crypto.decrypt(TEXT[:encrypted], :output => buffer)
+      GPGME::Crypto.new.decrypt(TEXT[:encrypted], :output => buffer)
       assert_equal TEXT[:plain], buffer.read
     end
 
@@ -95,17 +120,18 @@ describe GPGME do
 
     it "raises DecryptFailed when the decrypting key isn't available" do
       assert_raises GPGME::Error::DecryptFailed do
-        GPGME::Crypto.decrypt(TEXT[:unavailable])
+        GPGME::Crypto.new.decrypt(TEXT[:unavailable])
       end
     end
   end
 
   describe :sign do
     it "signs normal strings" do
+      crypto = GPGME::Crypto.new
       signatures = 0
-      sign = GPGME::Crypto.sign "Hi there"
+      sign = crypto.sign "Hi there"
 
-      GPGME::Crypto.verify(sign) do |signature|
+      crypto.verify(sign) do |signature|
         assert_instance_of GPGME::Signature, signature
         signatures += 1
       end
