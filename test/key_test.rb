b2b7fc9f1827bf7e7075e87bc9b9a17b18e16799
Peter Meier <peter.meier@immerda.ch>
2011-09-20 18:48:14 +0200

    Add find_exact method to lookup keys stricter
    GPG has the issue that finding a key for bar@example.com, also returns a key
    for foobar@example.com.
    This can be restricted by adding <> around the address: <bar@example.com>.
    Hence GPGME::Key.find_exact simply wraps <> around each email you passed to the method
    and delegates the rest to GPGME::Key.find

--- test/key_test.rb
+++ test/key_test.rb
@@ -53,6 +53,28 @@ describe GPGME::Key do
       assert keys.empty?
     end
   end
+  
+  describe :find_exact do
+    it "wraps an email address with angle brackets" do
+      GPGME::Key.expects(:find).with(:public,['<bar@example.com>'],[])
+      GPGME::Key.find_exact(:public,'bar@example.com')
+    end
+    
+    it "wraps multiple email addresses with angle brackets" do
+      GPGME::Key.expects(:find).with(:public,['<bar@example.com>','<foo@example.com>'],[])
+      GPGME::Key.find_exact(:public,['bar@example.com','foo@example.com'])
+    end
+    
+    it "does not touch other strings than email addresses" do
+      GPGME::Key.expects(:find).with(:public,['Bar Example'],[])
+      GPGME::Key.find_exact(:public,'Bar Example')
+    end
+    
+    it "does the same in mixed mode" do
+      GPGME::Key.expects(:find).with(:public,['<bar@example.com>','Foo Example'],[])
+      GPGME::Key.find_exact(:public,['bar@example.com','Foo Example'])
+    end
+  end
 
   describe :export do
     # Testing the lazy way with expectations. I think tests in
