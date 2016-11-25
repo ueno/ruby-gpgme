2e2c57a6ebfe2cf047b3e1e7d8a5a8a40f308990
Daiki Ueno <ueno@gnu.org>
2016-11-25 16:54:35 +0100

    Update tarballs

--- ext/gpgme/extconf.rb
+++ ext/gpgme/extconf.rb
@@ -62,7 +62,7 @@ EOS
   require 'rubygems'
   require 'mini_portile2'
 
-  libgpg_error_recipe = MiniPortile.new('libgpg-error', '1.24').tap do |recipe|
+  libgpg_error_recipe = MiniPortile.new('libgpg-error', '1.25').tap do |recipe|
     recipe.target = File.join(ROOT, "ports")
     recipe.files = ["ftp://ftp.gnupg.org/gcrypt/#{recipe.name}/#{recipe.name}-#{recipe.version}.tar.bz2"]
     recipe.configure_options = [
@@ -96,7 +96,7 @@ EOS
     recipe.activate
   end
 
-  gpgme_recipe = MiniPortile.new('gpgme', '1.7.0').tap do |recipe|
+  gpgme_recipe = MiniPortile.new('gpgme', '1.8.0').tap do |recipe|
     recipe.target = File.join(ROOT, "ports")
     recipe.files = ["ftp://ftp.gnupg.org/gcrypt/#{recipe.name}/#{recipe.name}-#{recipe.version}.tar.bz2"]
     recipe.configure_options = [
