64af9cadb94cc3c3d443849c6818bb73a693d2e2
Vicent Martí <vicent@github.com>
2020-01-25 07:40:09 +0100

    ctx: do not autoclose in `path_function` (#135)
    The default callback for writing a password to GPGME takes a file
    descriptor and needs to write the given password to it. By creating a
    new `IO` object directly on the file descriptor, the object will take
    ownership of it. This is a problem because the file descriptor passed to
    this callback will be explicitly closed by GPGME, and eventually, the
    `IO` allocated in `self.pass_function` will be garbage collected,
    closing the file descriptor again.
    
    Usually, closing a file descriptor that has already been closed results
    in a silent `EBADFD`, but a serious race condition can happen if the
    Kernel allocates the same file descriptor (e.g. through another `open`
    call in another part of your Ruby program): when GC triggers, the
    FD will be closed unexpectedly.
    
    To prevent this, simply set `IO#autoclose = false` on the `IO` object:
    the garbage collector will no longer close the file descriptor on
    cleanup, so the descriptor will only be closed once (in the explicit
    close call performed by us).

--- lib/gpgme/ctx.rb
+++ lib/gpgme/ctx.rb
@@ -517,6 +517,7 @@ keylist_mode=#{KEYLIST_MODE_NAMES[keylist_mode]}>"
 
     def self.pass_function(pass, uid_hint, passphrase_info, prev_was_bad, fd)
       io = IO.for_fd(fd, 'w')
+      io.autoclose = false
       io.puts pass
       io.flush
     end
